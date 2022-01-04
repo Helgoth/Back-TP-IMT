96b4cba3a022d8fe648a9644d5c1673fccbfeb51
Helgoth <anthony.rousseau@etu.imt-nord-europe.fr>
2022-01-04 17:30:01 +0100

    first commit

---
+++ src/test/java/fr/rousseau/TPannuaire/TPannuaireApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.rousseau.TPannuaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TPannuaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
